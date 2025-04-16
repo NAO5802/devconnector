dba1667e8a27367f22b48ff35972f889a483a5e9
NAO5802 <i100185b@gmail.com>
2025-04-16 17:34:20 +0900

    Add routing by react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,24 @@ import { Fragment } from 'react';
 import './App.css';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './components/layout/auth/Login';
+import { Register } from './components/layout/auth/Register';
 
 const App = () => (
-  <div className='App'>
+  <BrowserRouter>
     <Fragment>
       <Navbar />
-      <Landing />
+      <section className='container'>
+        <Routes>
+          {/* // TODO: Landingだけcontainerに入れないようにする */}
+          <Route path='/' element={<Landing />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </section>
     </Fragment>
-  </div>
+  </BrowserRouter>
 );
 
 export default App;
