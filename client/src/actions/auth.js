453b0b800ad7d9825622aeecbf32a630437572bf
NAO5802 <i100185b@gmail.com>
2025-04-22 15:46:09 +0900

    Create login logic

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import {
   AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
   REGISTER_FAIL,
   REGISTER_SUCCESS,
   USER_LOADED,
@@ -45,6 +47,7 @@ export const register =
         type: REGISTER_SUCCESS,
         payload: res.data,
       });
+      dispatch(loadUser());
     } catch (err) {
       const errors = err.response.data.errors;
 
@@ -54,5 +57,36 @@ export const register =
         });
       }
       dispatch({ type: REGISTER_FAIL });
+    }
+  };
+
+export const login =
+  ({ email, password }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const body = JSON.stringify({ email, password });
+    try {
+      const res = await axios.post('/api/auth', body, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+      dispatch(loadUser());
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => {
+          dispatch(setAlert(error.msg, 'danger'));
+        });
+      }
+
+      dispatch({ type: LOGIN_FAIL });
     }
   };
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,5 +1,7 @@
 import {
   AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
   REGISTER_FAIL,
   REGISTER_SUCCESS,
   USER_LOADED,
@@ -19,10 +21,12 @@ export default function auth(state = initialState, action) {
     case USER_LOADED:
       return { ...state, isAuthenticated: true, loading: false, user: payload };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return { ...state, token: null, isAuthenticated: false, loading: false };
     default:
