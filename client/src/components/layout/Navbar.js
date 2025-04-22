87792ea1c09c020d730e6212126a5d74587517e6
NAO5802 <i100185b@gmail.com>
2025-04-22 16:28:23 +0900

    Implement logout logic

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logout } from '../../actions/auth';
 
 export const Navbar = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+  };
+
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={onLogout} href='!#'>
+          <i className='fas fa-sign-out-alt'></i>{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <a href='!#'>Developers</a>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className='navbar bg-dark'>
       <h1>
@@ -9,17 +42,9 @@ export const Navbar = () => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      <ul>
-        <li>
-          <a href='!#'>Developers</a>
-        </li>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-      </ul>
+      {!auth.loading && (
+        <Fragment>{auth.isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
