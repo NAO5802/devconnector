bfe9297af982855041ecfd77bb8bc94913268816
NAO5802 <i100185b@gmail.com>
2025-04-22 14:32:18 +0900

    Set token to header on mounted

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,39 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import './App.css';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './components/layout/auth/Login';
 import { Register } from './components/layout/auth/Register';
+import setAuthToken from './utils/setAuthToken';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
 
-const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </Fragment>
-    </BrowserRouter>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,9 @@
-import { REGISTER_FAIL, REGISTER_SUCCESS } from '../actions/types';
+import {
+  AUTH_ERROR,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -11,10 +16,13 @@ export default function auth(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+      return { ...state, isAuthenticated: true, loading: false, user: payload };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return { ...state, token: null, isAuthenticated: false, loading: false };
     default:
