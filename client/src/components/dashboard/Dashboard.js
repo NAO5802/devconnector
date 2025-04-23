b2edc125e9e8d7f20cd91e37c2aa88f9570400b6
NAO5802 <i100185b@gmail.com>
2025-04-23 16:31:59 +0900

    Create dashboard reducers and actions

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+import Alert from '../layout/Alert';
 
-const Dashboard = (props) => {
+const Dashboard = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  const profile = useSelector((state) => state.profile);
+
+  useEffect(() => {
+    dispatch(getCurrentProfile());
+  }, []);
+
   return <div className='container'>Dashboard</div>;
 };
-
-Dashboard.propTypes = {};
 
 export default Dashboard;
