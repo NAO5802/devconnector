e227283f593b757b7b26c8cffc6f67e0f5604a3c
NAO5802 <i100185b@gmail.com>
2025-04-24 17:22:48 +0900

    Remove unused return valiables

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -168,7 +168,7 @@ export const deleteEducation = (education_id) => async (dispatch) => {
 
 export const deleteAccount = () => async (dispatch) => {
   try {
-    const res = await axios.delete('/api/profile');
+    await axios.delete('/api/profile');
 
     dispatch({ type: CLEAR_PROFILE });
     dispatch({ type: ACCOUNT_DELETED });
